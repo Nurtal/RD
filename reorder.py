7d2766671320219071b02163a2aac1a56356df02
Nurtal <nathan.foulquier.pro@gmail.com>
2016-11-24 11:15:20 +0100

    fusion panel function

--- reorder.py
+++ reorder.py
@@ -107,19 +107,21 @@ def restore_Data():
 
 def fusion_panel(listOfPanels):
 	"""
-	IN PROGRESS
+	-> Concat patient files, use all panel present in listOfPanels
+	-> Delete all files in FUSION folder
+	-> write new files in FUSION folder from
+	   files present in listOfPanels folders
+	-> Clean new files (remove header lines from other files)
+	-> reject a file if the patient is not present in all directory
+	   of listOfPanels
 
-	-> not functionnal for now
-		- write multiple header in FUSION file (TO TEST with new solution)
-
-	-> concat patient files, use all panel present in listOfPanels
 	-> listOfPanels is a list of string
-
-	TODO:
-		-> test on real data
-
 	"""
 
+	# Nettoyer le dossier FUSION
+	listOfFilesToClean = glob.glob("DATA/FUSION/*.csv")
+	for fileToClean in listOfFilesToClean:
+		os.remove(fileToClean)
 
 	# initialiser les files
 	listOfPatientFiles = glob.glob("DATA/"+str(listOfPanels[0])+"/*.csv")
@@ -128,14 +130,13 @@ def fusion_panel(listOfPanels):
 				patientFileInArray = patientFile.split("/")
 		elif(platform.system() == "Windows"):
 			patientFileInArray = patientFile.split("\\")
-		
+
 		patientFileName = patientFileInArray[-1]
 		patientFileNameInArray = patientFileName.split("_")
-		patientFileName = patientFileNameInArray[0]+"_"+patientFileNameInArray[1]+"_"+patientFileNameInArray[2]+"_"+patientFileNameInArray[3]+".csv"
+		patientFileName = patientFileNameInArray[0]+"_"+patientFileNameInArray[1]+"_"+"CENTER"+"_"+"DATE"+".csv"
 		newPatientFileName = "DATA/FUSION/"+str(patientFileName)
 		newFile = open(newPatientFileName, "w")
 		newFile.close()
-
 
 	# remplir le fichier
 	for panel in listOfPanels:
@@ -148,7 +149,7 @@ def fusion_panel(listOfPanels):
 			patientFileName = patientFileInArray[-1]
 
 			patientFileNameInArray = patientFileName.split("_")
-			patientFileName = patientFileNameInArray[0]+"_"+patientFileNameInArray[1]+"_"+patientFileNameInArray[2]+"_"+patientFileNameInArray[3]+".csv"
+			patientFileName = patientFileNameInArray[0]+"_"+patientFileNameInArray[1]+"_"+"CENTER"+"_"+"DATE"+".csv"
 			newPatientFileName = "DATA/FUSION/"+str(patientFileName)
 
 			sourceFile = open(patientFile, "r")
@@ -156,22 +157,20 @@ def fusion_panel(listOfPanels):
 			for line in sourceFile:
 				dataToCopy.append(line)
 			sourceFile.close()
-
 			destinationFile = open(newPatientFileName, "a")
 			for line in dataToCopy:
-
 				destinationFile.write(line)
 			destinationFile.close()
 
 	# Nettoyer les fichiers
+	# delete header from files used for
+	# the previous concatenation
 	listOfPatientFiles = glob.glob("DATA/FUSION/*.csv")
 	for patientFile in listOfPatientFiles:
 		patientFile_save = str(patientFile)+"_save.tmp"
-		shutil.copy(patientSaved, "DATA/FUSION/"+str(patientFile_save))
-
+		shutil.copy(patientFile, str(patientFile_save))
 	for patientFile in listOfPatientFiles:
 		patientFile_save = str(patientFile)+"_save.tmp"
-		
 		dataToInspect = open(patientFile_save, "r")
 		dataToCorrect = open(patientFile, "w")
 		cmpt = 0
@@ -182,16 +181,51 @@ def fusion_panel(listOfPanels):
 			if(cmpt == 1):
 				correctedData.append(line)
 			else:
-				if(lineInArray[1] != "POPULATION")
+				if(lineInArray[1] != "POPULATION"):
 					correctedData.append(line)
-
-		dataToCorrect.write(correctedData)
+		for line in correctedData:
+			dataToCorrect.write(line)
 
 		dataToCorrect.close()
 		dataToInspect.close()
+		os.remove(patientFile_save)
 		
 
+	# Trier les fichiers
+	# Verifier que le patient apparait bien dans
+	# tout les panels a fusionner
+	listOfPatientFiles = glob.glob("DATA/FUSION/*.csv")
+	for patientFile in listOfPatientFiles:
+		rejected = 0
+		if(platform.system() == "Linux"):
+				patientFileInArray = patientFile.split("/")
+		elif(platform.system() == "Windows"):
+			patientFileInArray = patientFile.split("\\")
+		nameInArray = patientFileInArray[1]
+		nameInArray = nameInArray.split("_")
+		patient_id = nameInArray[1]
 
+		for panel in listOfPanels:
+			if(not rejected):
+				listOfPatientIdInPanel =[]		
+				listOfPatientFilesInPanel = glob.glob("DATA/"+str(panel)+"/*.csv")
+				
+				for patientFileInPanel in listOfPatientFilesInPanel:
+					if(platform.system() == "Linux"):
+						patientFileInArrayInPanel = patientFileInPanel.split("/")
+					elif(platform.system() == "Windows"):
+						patientFileInArrayInPanel = patientFileInPanel.split("\\")
+					nameInArrayInPanel = patientFileInArrayInPanel[1]
+					nameInArrayInPanel = nameInArrayInPanel.split("_")
+					patient_idInPanel = nameInArrayInPanel[1]
+					listOfPatientIdInPanel.append(patient_idInPanel)
+
+				if(patient_id not in listOfPatientIdInPanel):
+					#print str(patient_id) + "=> not in panel " +str(panel)
+					print str(patient_id) + " rejected"
+					shutil.copy(patientFile, "DATA/REJECTED/")
+					os.remove(patientFile)
+					rejected = 1
 
 
 
@@ -234,10 +268,44 @@ def check_patient():
 				if("N" in str(parameterValue)):
 					rejected = 1
 
+		patientData.close()
+
 		if(rejected):
 			print patient_id + " rejected"
 			shutil.copy(patient, "DATA/REJECTED/")
 			os.remove(patient)
 
-		patientData.close()
+		
 
+def clean_folders(folder):
+	"""
+	-> remove all .csv file in folder
+	-> folder is a string, could be:
+		- PATIENT
+		- VECTOR
+		- FUSION
+		- ALL
+	"""
+
+	if(folder == "ALL"):
+		# clean PATIENT folder
+		listOfPatientFiles = glob.glob("DATA/PATIENT/*.csv")
+		for patientFile in listOfPatientFiles:
+			os.remove(patientFile)
+
+		# clean VECTOR folder
+		listOfPatientFiles = glob.glob("DATA/VECTOR/*.csv")
+		for patientFile in listOfPatientFiles:
+			os.remove(patientFile)
+
+		# clean FUSION folder
+		listOfPatientFiles = glob.glob("DATA/FUSION/*.csv")
+		for patientFile in listOfPatientFiles:
+			os.remove(patientFile)
+	else:
+		# clean PATIENT folder
+		listOfPatientFiles = glob.glob("DATA/"+str(folder)+"/*.csv")
+		for patientFile in listOfPatientFiles:
+			os.remove(patientFile)
+
+	
