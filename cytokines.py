a84fcbe23cbd420c43cbe891eda0640f0f17c2ce
Nurtal <nathan.foulquier.pro@gmail.com>
2016-12-16 16:27:43 +0100

    biplot

--- cytokines.py
+++ cytokines.py
@@ -319,17 +319,199 @@ def plot_explainedVariance(cohorte):
 
 """TEST SPACE"""
 
+# Create Data
 #CreateMatrix()
 #extractBinaryMatrix()
 #extractQuantitativeMatrix()
 data = AssembleMatrixFromFile()
 data = preprocessing.robust_scale(data)
-#y = get_discreteLabel()
 cohorte = filter_outlier(data)
+#y = get_discreteLabel()
 
+
+matrix_cleaned = open("DATA/CYTOKINES/myTestMatrix.csv", "w")
+
+# write header
+input_matrix = open("DATA/CYTOKINES/quantitativeMatrix.csv", "r")
+cmpt = 0
+for line in input_matrix:
+	if(cmpt == 0):
+		header = ""
+		for param in line:
+			header = str(param) + ","
+		matrix_cleaned.write(header[:-1]+"\n")
+	cmpt += 1
+input_matrix.close()
+
+
+for patient in cohorte:
+	lineToWrite = ""
+	for scalar in patient:
+		lineToWrite = lineToWrite + str(scalar) +","
+	matrix_cleaned.write(str(lineToWrite[:-1])+"\n")
+matrix_cleaned.close()
+
+
+
+
+
+"""
+
+# Perform PCA
+pca = PCA()
+pca.fit(cohorte)
 plot_explainedVariance(cohorte)
+cohorteInNewSpace = pca.fit_transform(cohorte)
 
-#quickClustering(data, 2, "cytokineTest.png")
-#quickPCA(cohorte, y, ["Male","Female"], "2d", "cytokinesPcaTest.png", 1, 1)
+# keep only the first 10 parameters
+cohorte_reduced = []
+for patient in cohorteInNewSpace:
+	newPatient = patient[:10]
+	cohorte_reduced.append(newPatient)
+cohorte_reduced = np.array(cohorte_reduced)
+
+quickClustering(cohorte_reduced, 4, "cytokineTest.png")
+#quickPCA(cohorte_reduced, y, ["Male","Female"], "2d", "cytokinesPcaTest.png", 1, 1)
+"""
 
 
+
+"""
+
+import pandas as pd
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+
+## import data
+
+my_csv = 'DATA/CYTOKINES/myTestMatrix.csv' ## path to your dataset
+
+dat = pd.read_csv(my_csv, index_col=0)
+# if no row or column titles in your csv, pass 'header=None' into read_csv
+# and delete 'index_col=0' -- but your biplot will be clearer with row/col names
+
+
+
+## perform PCA
+n = len(dat.columns)
+
+pca = PCA(n_components = n)
+# defaults number of PCs to number of columns in imported data (ie number of
+# features), but can be set to any integer less than or equal to that value
+
+pca.fit(dat)
+
+
+
+## project data into PC space
+
+# 0,1 denote PC1 and PC2; change values for other PCs
+xvector = pca.components_[0] # see 'prcomp(my_data)$rotation' in R
+yvector = pca.components_[1]
+
+xs = pca.transform(dat)[:,0] # see 'prcomp(my_data)$x' in R
+ys = pca.transform(dat)[:,1]
+
+
+
+## visualize projections
+    
+## Note: scale values for arrows and text are a bit inelegant as of now,
+##       so feel free to play around with them
+
+for i in range(len(xvector)):
+# arrows project features (ie columns from csv) as vectors onto PC axes
+    plt.arrow(0, 0, xvector[i]*max(xs), yvector[i]*max(ys),
+              color='r', width=0.0005, head_width=0.0025)
+    plt.text(xvector[i]*max(xs)*1.2, yvector[i]*max(ys)*1.2,
+             list(dat.columns.values)[i], color='r')
+
+for i in range(len(xs)):
+# circles project documents (ie rows from csv) as points onto PC axes
+    plt.plot(xs[i], ys[i], 'bo')
+    plt.text(xs[i]*1.2, ys[i]*1.2, list(dat.index)[i], color='b')
+
+plt.show()
+
+
+"""
+
+
+
+
+
+
+
+
+
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+ 
+# ## Data Import
+ 
+my_csv = 'DATA/CYTOKINES/iris.csv' ## path to your dataset
+ds = pd.read_csv(my_csv)
+ds.head()
+ 
+ 
+# ## Normalizing the data
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+ 
+ 
+# ## Create a data frame for feature sets ONLY
+X = scale.fit_transform(ds.drop('class', axis = 1)) # drop the label and normalizing
+X = pd.DataFrame(df) # It is required to have X converted into a data frame to later plotting need
+X.columns = [['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+X.head()
+ 
+# ## Run PCA against feature set dataframe
+ 
+ 
+n = len(X.columns)-1 # set the number
+pca = PCA(n_components = n)
+X_pca = pca.fit(X).transform(X)
+ 
+df_pca = pd.DataFrame(X_pca)
+df_pca['y'] = pd.DataFrame(y)
+df_pca.columns = [['pc1', 'pc2', 'pc3', 'y']]
+df_pca.head()
+ 
+ 
+ 
+# ## Biplot - Leverage `seaborn` package
+ 
+# In[83]:
+ 
+import seaborn as sns
+ 
+ 
+# Scatter plot based and assigne color based on 'label - y'
+sns.lmplot('pc1', 'pc2', data=df_pca, fit_reg = False, hue = 'y', size = 15, scatter_kws={"s": 100})
+ 
+# set the maximum variance of the first two PCs
+# this will be the end point of the arrow of each **original features**
+xvector = pca.components_[0]
+yvector = pca.components_[1]
+ 
+# value of the first two PCs, set the x, y axis boundary
+xs = pca.transform(X)[:,0]
+ys = pca.transform(X)[:,1]
+ 
+## visualize projections
+ 
+## Note: scale values for arrows and text are a bit inelegant as of now,
+##       so feel free to play around with them
+for i in range(len(xvector)):
+    # arrows project features (ie columns from csv) as vectors onto PC axes
+    # we can adjust length and the size of the arrow
+    plt.arrow(0, 0, xvector[i]*max(xs), yvector[i]*max(ys),
+              color='r', width=0.005, head_width=0.05)
+    plt.text(xvector[i]*max(xs)*1.1, yvector[i]*max(ys)*1.1,
+             list(df.columns.values)[i], color='r')
+ 
+for i in range(len(xs)):
+    plt.text(xs[i]*1.08, ys[i]*1.08, list(X.index)[i], color='b') # index number of each observations
+plt.title('PCA Plot of first PCs')
