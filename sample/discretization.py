201e1c0432d52cad7fa583f0831c63c846e9a363
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-02 16:21:11 +0100

    binaryzation

--- sample/discretization.py
+++ sample/discretization.py
@@ -4,25 +4,6 @@ discretization
 """
 
 
-# Brute discretization, according to binary values
-
-
-"""
-data = open(data_file_name, "r")
-cmpt = 0
-indexToVariableName = {}
-for line in data:
-	lineWithoutBackN = line.split("\n")
-	lineWithoutBackN = lineWithoutBackN[0]
-	lineInArray = lineWithoutBackN.split(";")
-	if(cmpt==0):
-		index = 0
-		for variable in lineInArray:	
-			variableToPossibleValueToBinaryValue[variable] = {}
-			index += 1
-data.close()
-"""
-
 
 def create_conversion_dict():
 	"""
@@ -88,11 +69,97 @@ def create_conversion_dict():
 
 
 
-# Algo :
-
-# 1 creation dict de conversion PossibleValueToBinaryValue
 
 
+def binarization(data_file_name, conversion_dict):
+	"""
+	-> Perform a "binarization" of the value present in data_file_name,
+	   with the dictionnary conversion_dict (create from the create_conversion_dict function)
+	-> create a discreteMatrix_imputed_binary file in CYTOKINES folder
 
-# 2 discretization avec le dict
+	-> TODO:
+		- clean code
+	"""
 
+	data = open(data_file_name, "r")
+	cmpt = 0
+	indexToVariableName = {}
+	indexOfVariableToDelete = []
+	identifiant_index = "undef"
+	variables_keep_index = []
+
+	# Select Column to Write
+	for line in data:
+		lineWithoutBackN = line.split("\n")
+		lineWithoutBackN = lineWithoutBackN[0]
+		lineInArray = lineWithoutBackN.split(";")
+		if(cmpt==0):
+			index = 0
+			for variable in lineInArray:
+
+				PossibleValueToBinaryValue = conversion_dict[variable]
+
+				# catch the identifiant variable
+				if(variable == "\Clinical\Sampling\OMICID"):
+					identifiant_index = index
+					variables_keep_index.append(identifiant_index)
+
+				# catch the variable with binary values in description file
+				if(len(PossibleValueToBinaryValue.values()) > 0):
+					# scan the binary values for ERROR entry
+					if("ERROR" not in PossibleValueToBinaryValue.values()):
+						variables_keep_index.append(index)
+
+				indexToVariableName[index] = variable			
+				index += 1
+		cmpt += 1
+	data.close()
+		
+
+	# Perform the conversion
+	data = open(data_file_name, "r")
+	data_converted = open("DATA/CYTOKINES/discreteMatrix_imputed_binary.csv", "w")
+	cmpt =0
+	header_new = ""
+	for line in data:
+		lineWithoutBackN = line.split("\n")
+		lineWithoutBackN = lineWithoutBackN[0]
+		lineInArray = lineWithoutBackN.split(";")
+
+		line_new = ""
+
+		if(cmpt == 0):
+			index = 0
+			for variable in lineInArray:
+				if(index in variables_keep_index):
+					header_new += variable +";" 
+				index += 1
+			header_new = header_new[:-1]
+			data_converted.write(header_new+"\n")
+
+
+		else:
+			index = 0
+			for scalar in lineInArray:
+				variable_name = indexToVariableName[index]
+				if(index in variables_keep_index):
+					PossibleValueToBinaryValue = conversion_dict[variable_name]
+					if(index != identifiant_index):
+						scalar_new = PossibleValueToBinaryValue[scalar]
+					else:
+						scalar_new = scalar
+
+					line_new += scalar_new +";"
+
+				index += 1
+			line_new = line_new[:-1]
+			data_converted.write(line_new+"\n")
+		cmpt += 1
+	data_converted.close()
+	data.close()
+
+
+
+# TEST SPACE
+#machin = create_conversion_dict()
+#binarization("DATA/CYTOKINES/discreteMatrix_imputed.csv", machin)
