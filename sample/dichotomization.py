19bf076c0171cdace287e7cd10c0410e118b43dd
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-10 18:05:19 +0100

    stuff

---
+++ sample/dichotomization.py
@@ -0,0 +1,12 @@
+"""
+dichotomization test
+for RD
+"""
+
+data = [[45 10 23], [21,12,87],[87,2,56]]
+
+# Algo
+# -> Pour chaque variable calculer:
+# 		- medianne
+#		- ecart type
+# 		-> Creation tableau disjonctif
