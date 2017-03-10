1152487f23ca9b79be189ccd547feaef2bd6f9ad
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-10 18:37:52 +0100

    dichotomization.py

--- sample/dichotomization.py
+++ sample/dichotomization.py
@@ -2,11 +2,26 @@
 dichotomization test
 for RD
 """
+import scipy.stats
+import numpy
+import math
 
-data = [[45 10 23], [21,12,87],[87,2,56]]
+# data test
+data = numpy.array([[45, 10, 23], [21,12,87],[87,2,56]])
 
 # Algo
 # -> Pour chaque variable calculer:
 # 		- medianne
 #		- ecart type
 # 		-> Creation tableau disjonctif
+#			-> Nombre de "case" dans le tableau
+
+
+
+for variable in data.transpose():
+	description = scipy.stats.describe(variable)
+	mean = description[2]
+	variance = description[3]
+	ecart_type = math.sqrt(variance)
+
+	# Creation du tableau
