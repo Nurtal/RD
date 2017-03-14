aa4541d66bf8160a8b7eee0d8787c0df9f18b91b
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-14 17:04:20 +0100

    R update

--- sample/SCRIPTS/features_extraction.R
+++ sample/SCRIPTS/features_extraction.R
@@ -2,11 +2,18 @@
 library(Boruta)
 
 # load data
-flow_data <- read.csv("C:\\Users\\PC_immuno\\Desktop\\Nathan\\SpellCraft\\RD\\sample\\DATA\\MATRIX\\flowcyto.txt", header = T, stringsAsFactors = F, sep="\t")
-diagnosic_table = read.csv("C:\\Users\\PC_immuno\\Desktop\\Nathan\\SpellCraft\\RD\\sample\\DATA\\patientIndex.csv", header = F, sep=";")
+flow_data <- read.csv("/home/foulquier/Bureau/SpellCraft/WorkSpace/FeatureSelection/flowcyto.txt", header = T, stringsAsFactors = F, sep="\t")
+diagnosic_table = read.csv("/home/foulquier/Bureau/SpellCraft/WorkSpace/Github/RD/sample/DATA/patientIndex.csv", header = F, sep=";")
 colnames(diagnosic_table) <- c("OMICID", "diagnostic")
 
-df <- diagnosic_table
+rownames(diagnosic_table) <- diagnosic_table$OMICID
+rownames(flow_data) <- flow_data$OMICID
+diagnosic_table$OMICID <- NULL
+flow_data <- merge(diagnosic_table, flow_data, by=0)
+
+
+# convert disease to 1, control to 0
+df <- flow_data
 m <- as.matrix(df)
 m[m=="Control"] <- 0
 m[m=="SLE"] <- 1
@@ -17,20 +24,10 @@ m[m=="SSc"] <- 1
 m[m=="SjS"] <- 1
 m[m=="PAPs"] <- 1
 m[m=="MCTD"] <- 1
-diagnosic_table <- as.data.frame(m)
-
-
-rownames(diagnosic_table) <- diagnosic_table$OMICID
-rownames(flow_data) <- flow_data$OMICID
-
-flow_data$OMICID <- NULL
-diagnosic_table$OMICID <- NULL
-
-
-flow_data <- merge(diagnosic_table, flow_data, by=0)
-
+flow_data <- as.data.frame(m)
 
 # Delete some variables
+flow_data$Row.names <- NULL
 flow_data$P1_CD14LOWCD16POS_NONCLASSICMONOCYTES <- NULL
 flow_data$P2_DRNEGCD123POS_BASOPHILS <- NULL
 flow_data$P2_LINNEGDRPOSCD11CNEGCD123POS_PDC <- NULL
@@ -42,19 +39,13 @@ flow_data<-flow_data[!(traindata$OMICID=="32151646"),]
 flow_data<-flow_data[!(traindata$OMICID=="32151888"),]
 
 # Split into Panel
-flow_data_panel_1 <- flow_data[,grep("P1_|OMICID", colnames(flow_data))]
-flow_data_panel_2 <- flow_data[,grep("P2_", colnames(flow_data))]
-flow_data_panel_3 <- flow_data[,grep("P3_", colnames(flow_data))]
-flow_data_panel_4 <- flow_data[,grep("P4_", colnames(flow_data))]
-flow_data_panel_5 <- flow_data[,grep("P5_", colnames(flow_data))]
-flow_data_panel_6 <- flow_data[,grep("P6_", colnames(flow_data))]
+flow_data_panel_1 <- flow_data[,grep("P1_|diagnostic|OMICID", colnames(flow_data))]
+flow_data_panel_2 <- flow_data[,grep("P2_|diagnostic|OMICID", colnames(flow_data))]
+flow_data_panel_3 <- flow_data[,grep("P3_|diagnostic|OMICID", colnames(flow_data))]
+flow_data_panel_4 <- flow_data[,grep("P4_|diagnostic|OMICID", colnames(flow_data))]
+flow_data_panel_5 <- flow_data[,grep("P5_|diagnostic|OMICID", colnames(flow_data))]
+flow_data_panel_6 <- flow_data[,grep("P6_|diagnostic|OMICID", colnames(flow_data))]
 
-
-flow_data_panel_2 <- flow_data[,grep("P2_|OMICID", colnames(flow_data))]
-flow_data_panel_2 <- merge(flow_data_panel_2, diagnosic_table, by="OMICID")
-
-flow_data_panel_1 <- merge(flow_data_panel_1, diagnosic_table, by="OMICID")
-flow_data_panel_1$OMICID <- NULL
 
 # Remove NA or perform Imputation
 flow_data_panel_1 <- flow_data_panel_1[complete.cases(flow_data_panel_1),]
@@ -66,12 +57,10 @@ flow_data_panel_6 <- flow_data_panel_6[complete.cases(flow_data_panel_6),]
 
 
 # Panel to Process:
-panel_to_process <- flow_data_panel_1
-names(panel_to_process)[12]
+panel_to_process <- flow_data_panel_6
 
 # Run Boruta
-f <- paste(names(panel_to_process)[1], "~.", sep="")
-boruta.train <- Boruta(diagnostic~., data = panel_to_process, doTrace = 2)
+boruta.train <- Boruta(diagnostic~.-OMICID, data = panel_to_process, doTrace = 2)
 
 
 # Display results
@@ -89,7 +78,7 @@ axis(side = 1,las=2,labels = names(Labels),
 final.boruta <- TentativeRoughFix(boruta.train)
 
 # Display supplemental results
-png("feature_selection_panel_6.png", width=4, height=4, units="in", res=300)
+png("/home/foulquier/Bureau/SpellCraft/WorkSpace/FeatureSelection/feature_selection_panel_1.png", width=4, height=4, units="in", res=300)
 plot(final.boruta, xlab = "", xaxt = "n")
 lz<-lapply(1:ncol(boruta.train$ImpHistory),function(i)
   boruta.train$ImpHistory[is.finite(boruta.train$ImpHistory[,i]),i])
@@ -103,5 +92,12 @@ dev.off()
 # Final steps
 getSelectedAttributes(final.boruta, withTentative = F)
 boruta.df <- attStats(final.boruta)
-write.table(boruta.df, "feature_selection_panel_6.txt", sep=";")
+write.table(boruta.df, "/home/foulquier/Bureau/SpellCraft/WorkSpace/FeatureSelection/feature_selection_panel_1.txt", sep=";")
 
+# Write new tables
+selected_variables <- getSelectedAttributes(final.boruta, withTentative = F)
+f <- paste(selected_variables, collapse='|')
+f <- paste(c(f, "diagnostic|OMICID"), collapse = '|')
+panel_to_process <- panel_to_process[,grep(f, colnames(panel_to_process))]
+
+write.table(panel_to_process, "/home/foulquier/Bureau/SpellCraft/WorkSpace/FeatureSelection/panel_6_filtered.txt", sep=";")
