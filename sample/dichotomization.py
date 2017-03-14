84d72e409bb6759c98fb7c878c77937f2d27bd7a
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-14 16:56:57 +0100

    dichotomisation update

--- sample/dichotomization.py
+++ sample/dichotomization.py
@@ -8,6 +8,76 @@ import math
 
 
 
+
+def extract_matrix_from(file_name):
+	"""
+	-> extract a matrix (numpy.array object)
+	   from the file file_name
+	-> Used to create matrix for the dichotomization process
+	-> All parameters except for OMICID
+	   and diagnostic are present in the matrix
+	-> return a tuple (matrix, index_to_variable, row_to_patient)
+	   where:
+	   		-index_to_variable is a dict, position in a vector to
+	         the name of the corresponding variable.
+	        -row_to_patient is a dict, postion in matrix (row, i.e vector index)
+	         to the OMICID of the corresponding patient.
+	"""
+
+	index_to_variable = {}
+	row_to_patient = {}
+	OMICID_index = "undef"
+	exclude_list = ["OMICID", "diagnostic"]
+	matrix = []
+
+	# initialise index to variable dictionnary
+	# create matrix without OMICID and diagnostic
+	file_data = open(file_name, "r")
+	cmpt_line = 0
+	for line in file_data:
+		line = line.split("\n")
+		line = line[0]
+		line_array = line.split(";")
+		if(cmpt_line == 0):
+			cmpt_index = 0
+			for scalar in line_array:
+				if(scalar not in exclude_list):
+					index_to_variable[cmpt_index] = scalar
+				if(scalar == "OMICID"):
+					OMICID_index = cmpt_index
+				cmpt_index += 1
+		else:
+			cmpt_index = 0
+			vector = []
+			for scalar in line_array:
+				if(cmpt_index in index_to_variable.keys()):
+					vector.append(float(scalar))
+				cmpt_index += 1
+			matrix.append(vector)
+		cmpt_line += 1
+	file_data.close()
+
+	matrix = numpy.array(matrix)
+
+	# initialise row to patient dictionnary
+	file_data = open(file_name, "r")
+	cmpt_line = 0
+	for line in file_data:
+		line = line.split("\n")
+		line = line[0]
+		line_array = line.split(";")
+		if(cmpt_line > 0):
+			patient_omicid = line_array[OMICID_index]
+			row_to_patient[cmpt_line] = patient_omicid
+		cmpt_line += 1
+	file_data.close()
+
+	return (matrix, index_to_variable, row_to_patient)
+
+
+
+
+
 def create_disjonct_table(variable, method, number_of_interval):
 	"""
 	-> build disjonctif table for a specific variable
@@ -133,12 +203,49 @@ def dichotomize(data, tables):
 	return data_new
 
 
+
+
+def save_dichotomized_matrix_in_file(index_to_variable, row_to_patient, data, number_of_steps, data_save_name):
+	"""
+	-> save dichotomized matrix in file data_save_name
+	-> index_to_variable is a dict, generated with the extract_matrix_from function
+	-> row_to_patient is a dict, generated with the extract_matrix_from function
+	-> data is the dichotomized matrix
+	-> number_of_steps is the number of interval used for the dichotomization
+	-> Used to prepare a file for the NN module
+	"""
+	
+	header = ""
+	data_save = open(data_save_name, "w")
+
+	for variables in index_to_variable.values():
+		for iteration in xrange(0, number_of_steps):
+			header += variables+"_ITERATION_"+str(iteration)+";"
+	header = header + "OMICID"
+
+	data_save.write(header+"\n")
+	cmpt_row = 1
+	for vector in data:
+		line_new = ""
+		cmpt_index = 0
+		for scalar in vector:
+			line_new += str(scalar) +";"
+			cmpt_index += 1
+
+		line_new += str(row_to_patient[cmpt_row])
+		data_save.write(line_new+"\n")
+		cmpt_row += 1 
+	data_save.close()
+
+
+
 """TEST SPACE"""
 data = numpy.array([[45, 10, 23,0], [21,12,87,5],[87,2,56,10]])
 variable = numpy.array([0,1,5,6,4,10])
 
-# Create disjonct table => OK
-table_test = create_disjonct_table(variable, "standard", 5)
+# Generate matrix from data file
+pack = extract_matrix_from("DATA/MATRIX/panel_1_filtered_processed.txt")
+data = pack[0]
 
 # create disjonct table for all variable in a matrix
 #	-> input : a matrix
@@ -149,3 +256,12 @@ tables_test = create_disjonctTable_for_matrix(data, 5)
 #	- use matrix and table as input
 #	- return a new matrix
 truc = dichotomize(data, tables_test)
+
+
+# save dichotomized matrix in a file to be processed by NN
+save_dichotomized_matrix_in_file(pack[1], pack[2], truc, 5, "DATA/MATRIX/data_dichotomized_test.csv")
+
+	
+
+
+
