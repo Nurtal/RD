1b904f67f25b0091f7cd8d734a0d0be591209668
Nurtal <nathan.foulquier.pro@gmail.com>
2017-03-17 10:16:40 +0100

    stach

--- sample/demo.py
+++ sample/demo.py
@@ -6,7 +6,6 @@ import describe
 import discretization
 import reorder
 import cytokines
-import analysis
 import sys
 
 
@@ -23,14 +22,14 @@ command = sys.argv[1]
 
 if(command == "case_1"):
 	# Preprocessing
-	panel = "PANEL_1"
+	panel = "PANEL_9"
 	print "=> " +str(panel)
 	reorder.clean_folders("ALL")
 	procedure.checkAndFormat("DATA/"+panel, "DATA/PATIENT")
 	#remove_typeOfParameter("PROPORTION")
 	reorder.remove_typeOfParameter("ABSOLUTE")
-	#reorder.remove_typeOfParameter("RATIO")
-	#reorder.remove_typeOfParameter("MFI")
+	reorder.remove_typeOfParameter("RATIO")
+	reorder.remove_typeOfParameter("MFI")
 	reorder.check_patient()
 	reorder.save_data()
 	procedure.show_PCA("DATA/PATIENT", "center", "3d", "IMAGES/test.png", "PROPORTION", 0, 1)
@@ -89,53 +88,5 @@ if(command == "prepare_data"):
 
 
 
-################
-# FAST CLUSTER #
-################
-if(command == "clustering"):
 
-	# Init parameters
-	data_file_name = "data_luminex.csv"
-	line_delimiter = " "
-	header_in_file = 1
-	number_of_cluster = 2
-	saveFile = "test.png"
-	matrix = []
 
-	# Parse file and create matrix
-	data_file = open(data_file_name, "r")
-	cmpt = 0
-	for line in data_file:
-		line_parsed = line.split("\n")
-		line_parsed = line_parsed[0]
-		line_array = line_parsed.split(line_delimiter)
-		vector = []
-		vector_can_be_add = 1
-
-		if(header_in_file and cmpt == 0):
-			print "[+] Pass header"
-		else:
-			index = 0
-			for scalar in line_array:
-				# pass the first column, used 
-				# for data imported from R
-				if(index != 0):
-					try:
-						scalar_int = float(scalar)
-						vector.append(float(scalar))
-					except:
-						print "[!] Can't cast "+ str(scalar) + " into int"
-						vector_can_be_add = 0
-				
-				index += 1
-
-			# Need at least 3 coordinates and all vectors
-			# have to be of the same size
-			if(len(vector) > 2 and vector_can_be_add):
-				matrix.append(vector)
-
-		cmpt += 1
-	data_file.close()
-
-	# Kmeans
-	analysis.quickClustering(matrix, number_of_cluster, saveFile)
